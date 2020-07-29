50045268e8e709ad3a8f99397355c26b2ff12eee
draumzinho <draorjv@gmail.com>
2020-07-29 14:18:09 -0300

    oba

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Logo from '../../assets/img/Logo.png';
+import { Link } from 'react-router-dom';
 import './Menu.css';
 import Button from '../Button';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="Tibiaflix"/>
-            </a>
-            <Button as="a" className="ButtonLink" href="/">
+            </Link>
+            <Button className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={() => (<div> Erro 404: Page not found.</div>)} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
